ae9ccf9611e2c22dccd5a6f06bb1d3291f1f9784
etherealelement <vitalik.safonov@list.ru>
2024-09-28 08:04:09 +0300

    delete font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
+import { Button } from "@/shared/ui/button";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Button variant="outline">Button</Button>
+    </div>
+  );
 }
