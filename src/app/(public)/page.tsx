706ea45966c73fbefcb6b7372624e1a3df59c798
etherealelement <vitalik.safonov@list.ru>
2024-09-29 15:32:32 +0300

    feat: create base map

---
+++ src/app/(public)/page.tsx
@@ -0,0 +1,11 @@
+import { HeaderPlaceholder } from "@/app/_widgets/header/header-placeholder";
+import { Button } from "@/shared/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <HeaderPlaceholder></HeaderPlaceholder>
+      <Button variant="outline">Button</Button>
+    </>
+  );
+}
